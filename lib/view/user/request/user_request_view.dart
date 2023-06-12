81007a3f85d5cdf7e656a8e524246f4a4b4a6b7f
DanishGujjar91 <danishasghar9263@gmail.com>
2023-06-13 01:48:56 +0500

    final

--- lib/view/user/request/user_request_view.dart
+++ lib/view/user/request/user_request_view.dart
@@ -2,7 +2,6 @@ import 'package:easy_pick/view/user/request/widgets/user_add_request_popup.dart'
 import 'package:easy_pick/view/user/request/widgets/user_request_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import '../../../models/user_model.dart';
 import '../../../repos/product_repo.dart';
 import '../../../repos/user_repo.dart';
